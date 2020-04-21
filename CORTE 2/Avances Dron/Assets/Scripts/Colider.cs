023d23deb465e128cff66656e3ad057a3b68f7fa
JoseFlorez7 <u1201883@unimilitar.edu.co>
2020-04-21 11:35:17 -0500

    Avances Dron

---
+++ CORTE 2/Avances Dron/Assets/Scripts/Colider.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colider : MonoBehaviour
+{
+    public Movimiento movimiento;
+    public GameObject obj;
+    private GameObject objCol;
+
+    float m1, m2;
+    float e;
+    float teta, fi, alfa;
+
+    Vector3 vel1;
+    Vector3 vel2;
+
+    Vector3 pos1;
+    Vector3 pos2;
+    static Vector3 scal1 = new Vector3(4f, 0.6f, 4.5f);
+    Vector3 scal2;
+
+    Vector3 max1, min1, max2, min2;
+    Vector3 dis;
+
+    float vp, vn, vt;
+    float vp2, vn2, vt2;
+    float v1prim, vx1prim, vy1prim, vz1prim;
+    float v2prim, vx2prim, vy2prim, vz2prim;
+
+    bool comp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objCol = GameObject.Find("AreaCol");
+
+        vel1 = new Vector3(0, 0, 0);
+        vel2 = new Vector3(0, 0, 0); 
+
+        pos1 = objCol.GetComponent<Transform>().position;
+        pos2 = obj.GetComponent<Transform>().position;
+        //scal1 = objCol.GetComponent<Transform>().localScale;
+        //scal1 = 
+        scal2 = obj.GetComponent<Transform>().localScale;
+
+        e = 0.98f;
+
+        m1 = 4f;
+        m2 = Mathf.Pow(scal2.x, 1) * 2;
+
+        vp = vn = vn2 = vp2 = 0;
+        v1prim = vy1prim = vx1prim = 0;
+
+        comp = true;
+
+        Debug.Log("masa1 =" + m1);
+        Debug.Log("masa2 =" + m2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        actualizar();
+        MaximosMinimos();
+        dete();
+
+        suelo();
+        mov();
+    }
+
+    void MaximosMinimos()
+    {
+        max1.x = pos1.x + (scal1.x/2f);
+        min1.x = pos1.x - (scal1.x/2f);
+        max1.y = pos1.y + (scal1.y/2f);
+        min1.y = pos1.y - (scal1.y/2f);
+        max1.z = pos1.z + (scal1.z/2f);
+        min1.z = pos1.z - (scal1.z/2f);
+
+        max2.x = pos2.x + (scal2.x / 2f);
+        min2.x = pos2.x - (scal2.x / 2f);
+        max2.y = pos2.y + (scal2.y / 2f);
+        min2.y = pos2.y - (scal2.y / 2f);
+        max2.z = pos2.z + (scal2.z / 2f);
+        min2.z = pos2.z - (scal2.z / 2f);
+
+    }
+
+    void mov()
+    {
+        pos2 += vel2;
+        vel2 = vel2 * e;
+        obj.GetComponent<Transform>().position = pos2;
+    }
+
+    void actualizar()
+    {
+        pos1 = objCol.GetComponent<Transform>().position;
+    }
+
+    void dete()
+    {
+
+        float de;
+        float de2;
+        float de3;
+        float de4;
+        float aux123;
+        int b = movimiento.a;
+
+        if ((min1.x < max2.x) && (max1.x > min2.x) && (min1.y < max2.y) && (max1.y > min2.y) && (min1.z < max2.z) && (max1.z > min2.z))
+        {
+            //Debug.Log("HAS PENETRADO");
+            vel1 = movimiento.vel;
+
+            de = Mathf.Sqrt(Mathf.Pow(pos2.x - pos1.x, 2) + Mathf.Pow(pos2.y - pos1.y, 2) + Mathf.Pow(pos2.z - pos1.z, 2));
+
+            dis.x = Mathf.Abs(pos2.x - pos1.x);
+            dis.y = Mathf.Abs(pos2.y - pos1.y);
+            dis.z = Mathf.Abs(pos2.z - pos1.z);
+
+            if(b == 3 || b == 4)
+            {
+                vel1.x = vel1.z;
+            }
+            if(b==5 || b== 6)
+            {
+                aux123 = vel1.x;
+                vel1.x = vel1.y;
+                vel1.y = aux123;
+            }
+
+            de2 = Mathf.Sqrt(Mathf.Pow(dis.x, 2) + Mathf.Pow(dis.z, 2));
+            teta = Mathf.Acos(dis.x / de2);
+
+            vp = vel1.x * Mathf.Cos(teta) + vel1.y * Mathf.Sin(teta);
+            vn = -vel1.x * Mathf.Sin(teta) + vel1.y * Mathf.Cos(teta);
+            
+            //------------------------
+            vp2 = vel2.x * Mathf.Cos(teta) + vel2.y * Mathf.Sin(teta);
+            vn2 = -vel2.x * Mathf.Sin(teta) + vel2.y * Mathf.Cos(teta);
+
+            v1prim = ((m1 - (e * m2)) / (m1 + m2)) * vel1.x + (((1 + e) * m2) / (m1 + m2)) * vel2.x;
+            vx1prim = v1prim * Mathf.Cos(teta) - vn * Mathf.Sin(teta);
+            vy1prim = v1prim * Mathf.Sin(teta) + vn * Mathf.Cos(teta);
+            //--------------------------
+            v2prim = ((m2 - (e * m1)) / (m1 + m2)) * vel2.x + (((1 + e) * m1) / (m1 + m2)) * vel1.x;
+            vx2prim = v2prim * Mathf.Cos(teta) - vn * Mathf.Sin(teta);
+            vy2prim = v2prim * Mathf.Sin(teta) + vn * Mathf.Cos(teta);
+
+            movimiento.vel = new Vector3(vx1prim, vy1prim, 0);
+            if (b == 3 || b == 4)
+            {
+                vel2 = (new Vector3(0, vy2prim, vx2prim)) * 0.001f;
+            }
+            else
+            {
+                vel2 = (new Vector3(vx2prim, vy2prim, 0)) * 0.001f;
+            }
+            if(b==5 || b == 6)
+            {
+                vel2 = (new Vector3(vy2prim, vx2prim, 0)) * 0.001f;
+            }
+        }
+    }
+
+    void suelo()
+    {
+        pos2.x += movimiento.runge_pos(pos2.x, vel2.x,1).x;
+        vel2.x += movimiento.runge_pos(pos2.x, vel2.x, 1).y;
+
+        pos2.y += movimiento.runge_pos(pos2.y, vel2.y, 2).x;
+        vel2.y += movimiento.runge_pos(pos2.y, vel2.y, 2).y;
+
+        pos2.z += movimiento.runge_pos(pos2.z, vel2.z, 3).x;
+        vel2.z += movimiento.runge_pos(pos2.z, vel2.z, 3).y;
+
+        if(pos2.y - (scal2.y/2) < 0)
+        {
+            vel2.y = vel2.y * -0.4f;
+        }
+    }
+}
+
---
+++ CORTE 2/Avances Dron/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,271 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    //VARIABLES
+
+    public Camera Cam;
+    private GameObject Dron = null;
+    Animator anim;
+    public GameObject Acol;
+
+    //VARIABLES DE KUTTA
+    float t = 0;
+    float K1, K2, K3, K4, L1, L2, L3, L4;
+    public float h = 0.005f;
+    public float g = 9.8f;
+
+    //VARIABLES DRON
+    Vector3 pos;
+    public Vector3 vel;
+    Vector3 acel;
+    Vector3 rot;
+    float v01;
+    bool piso = true;
+    public int a = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        Dron = GameObject.Find("avio_animado");
+        //pos = new Vector3(0, -0.221f, 0);
+        //Dron.GetComponent<Transform>().position = pos;
+        Cam.GetComponent<Transform>().position = new Vector3(-5f,2f,0);
+        pos = Dron.GetComponent<Transform>().position;
+        vel = new Vector3(0, 0, 0);
+        acel = new Vector3(2f, 2f, 2f);
+        v01 = 5f;
+        rot = new Vector3(0, 0, 0);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        mov();
+
+        Botones();
+        rotation();
+        colision();
+    }
+
+    void Botones()
+    {
+        if (Input.GetKey("w") || Input.GetKey("s")) {
+            if (Input.GetKey("w"))
+            {
+                if (vel.x < 500)
+                {
+                    vel.x += acel.x;
+                    rot.z += Time.deltaTime * -2.5f;
+                    a = 1;
+                }
+            }
+            if(Input.GetKey("s"))
+            {
+                if (vel.x > -400)
+                {
+                    vel.x -= acel.x;
+                    rot.z += Time.deltaTime * 2.5f;
+                    a = 2;
+                }
+            }
+        } else {
+            if (vel.x > 0)
+            {
+                rot.z += Time.deltaTime * 14f;
+                vel.x -= acel.x*8;
+            }
+            else
+            {
+                rot.z += Time.deltaTime * -14f;
+                vel.x += acel.x*8;
+            }
+        }
+
+
+        if (Input.GetKey("a") || Input.GetKey("d"))
+        {
+            if (Input.GetKey("a"))
+            {
+                if (vel.z < 420)
+                {
+                    rot.x += Time.deltaTime *2f;
+                    vel.z += acel.z;
+                    a = 3;
+                }
+            }
+            if (Input.GetKey("d"))
+            {
+                if(vel.z > -420)
+                {
+                    rot.x += Time.deltaTime * -2f;
+                    vel.z -= acel.z;
+                    a = 4;
+                }
+            }
+        }
+        else
+        {
+            if (vel.z > 0) {
+                rot.x += Time.deltaTime * -16f;
+                vel.z -= acel.z * 8;
+            }
+            else
+            {
+                rot.x += Time.deltaTime * 16f;
+                vel.z += acel.z * 8;
+            }
+            
+        }
+
+        if(Input.GetKey("space") || Input.GetKey("c"))
+        {
+           
+            if (Input.GetKey("space"))
+            {
+                anim.SetBool("mov", true);
+                if (vel.y < 45)
+                {
+                    vel.y += acel.y;
+                    a = 5;
+                }
+            }
+            if (Input.GetKey("c"))
+            {
+                if(vel.y > -45)
+                {
+                    vel.y -= acel.y;
+                    a = 6;
+                }
+            }
+        }
+        else
+        {
+            if (vel.y > 0)
+            {
+                vel.y -= acel.y;
+            }
+            else
+            {
+                vel.y += acel.y;
+            }
+        }
+
+        if (Input.GetKey("p"))
+        {
+            Start();
+        }
+    }
+
+    void rotation()
+    {
+
+        if (vel.z > -4 && vel.z < 4)
+        {
+            if (rot.x > 0)
+            {
+                rot.x += Time.deltaTime * -5f;
+            }
+            else
+            {
+                rot.x += Time.deltaTime * 5f;
+            }
+        }
+        if (vel.x > -4 && vel.x < 4)
+        {
+            if (rot.z > 0)
+            {
+                rot.z += Time.deltaTime * -5f;
+            }
+            else
+            {
+                rot.z += Time.deltaTime * 5f;
+            }
+        }
+        Dron.GetComponent<Transform>().localRotation = Quaternion.Euler(rot.x, rot.y, rot.z);
+    }
+
+    void mov()
+    {
+        //DETERMINANDO LAS POSICIONES Y VELOCIDADES EN LAS DIFERENTES DIRECCIONES
+        pos.x += runge_pos(pos.x, vel.x, 1).x;
+        vel.x += runge_pos(pos.x, vel.x, 1).y;
+
+        pos.y += runge_pos(pos.y, vel.y, 2).x;
+        vel.y += runge_pos(pos.y, vel.y, 2).y;
+
+        pos.z += runge_pos(pos.z, vel.z, 3).x;
+        vel.z += runge_pos(pos.z, vel.z, 3).y;
+
+
+        Cam.GetComponent<Transform>().position = new Vector3(pos.x - 6f, pos.y + 2f, pos.z);
+        Dron.GetComponent<Transform>().position = pos;
+
+        //Debug.Log("POs="+ pos);
+        //Debug.Log("Vel=" + vel);
+    }
+    
+    void colision()
+    {
+        //Area De Colision
+        float scale = Acol.GetComponent<Transform>().localScale.y;
+        if ((pos.y - scale) < 0 && piso)
+        {
+            vel.y = vel.y * -0.4f;
+            piso = false;
+        }
+        if(piso == false && pos.y > 1)
+        {
+            piso = true;
+        }
+
+        Vector3 a = pos;
+        Acol.GetComponent<Transform>().position = new Vector3(a.x, a.y, a.z);
+    }
+
+    public Vector2 runge_pos(float pos, float v, int cond)
+    {//EL METODO DE RUNGE HACE LAS RESPECTIVAS OPERACIONES DEL METODO MATEMATICO y RETORNA EL VALOR DE LA POSICION
+
+        K1 = h * v;
+        L1 = h * func(t, pos, v, cond);
+        K2 = h * (v + (L1 / 2));
+        L2 = h * func(t + (h / 2), pos + (K1 / 2), v + (L1 / 2), cond);
+        K3 = h * (v + (L2 / 2));
+        L3 = h * func(t + (h / 2), pos + (K2 / 2), v + (L2 / 2), cond);
+        K4 = h * (v + L3);
+        L4 = h * func(t + h, pos + K3, v + L3, cond);
+
+        t = t + h;
+
+        //DEVULVE LA POSICION CALCULADA POR EL METODO
+        pos = ((K1 + 2 * K2 + 2 * K3 + K4) / 6);
+        v = ((L1 + 2 * L2 + 2 * L3 + L4) / 6);
+
+        Vector2 res;
+        res = new Vector2(pos, v);
+
+        //RETORNA LA POSICION
+        return res;
+    }
+    float func(float t, float x1, float vx1, int cond)
+    {
+        if(cond <= 1)
+        {
+            return 0;
+        }
+        if(cond <= 2)
+        {
+            float res = -g;
+            return res;
+        }else
+        {
+            return 0;
+        }
+ 
+    }
+
+}
